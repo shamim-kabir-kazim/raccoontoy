b55bdf9803c9411e5bb821353f1756ea534bf22e
shamim-kabir-kazim <cabrio.hexen@gmail.com>
2025-09-23 14:51:45 +0600

    fr

--- src/components/product/CategoryShowing.jsx
+++ src/components/product/CategoryShowing.jsx
@@ -2,9 +2,6 @@ import React, { useRef } from "react";
 import "./CategoryShowing.css";
 import CatCard from "./CatCard";
 
-
-
-// Demo category items with real images
 const categories = [
   {
     id: 1,
@@ -13,63 +10,63 @@ const categories = [
   },
   {
     id: 2,
-    image: "https://i.postimg.cc/4N1J6y6Y/dc-action-figure.png",
+    image: "https://i.postimg.cc/658FgZ6p/Gemini-Generated-Image-pshaeapshaeapsha-1.png",
     title: "DC Action Figure"
   },
-    {
+  {
     id: 14,
-    image: "https://i.postimg.cc/8PP8wJQm/naruto-action-figure.png",
+    image: "https://i.postimg.cc/4xJ2Zhsc/O1-CN01-FX1d-JG1-OBz-Q0sm7dd-2214634361668-0-cib.png",
     title: "Anime Action Figure"
   },
   {
     id: 3,
-    image: "https://i.postimg.cc/8PP8wJQm/naruto-action-figure.png",
+    image: "https://i.postimg.cc/L6fhRnNm/Naruto-set.png",
     title: "Naruto Action Figure"
   },
   {
     id: 4,
-    image: "https://i.postimg.cc/C5n5r5vD/pokemon-action-figure.png",
+    image: "https://i.postimg.cc/wMDVh9cL/Gemini-Generated-Image-zbfbeozbfbeozbfb-removebg-preview-1.png",
     title: "Pokemon Action Figure"
   },
   {
     id: 5,
-    image: "https://i.postimg.cc/90n0kJ6v/one-piece-action-figure.png",
+    image: "https://i.postimg.cc/nzp76qvj/Gemini-Generated-Image-s39mcs39mcs39mcs-1-removebg-preview.png",
     title: "One Piece Action Figure"
   },
   {
     id: 6,
-    image: "https://i.postimg.cc/3w7wFz0m/dragon-ballz-action-figure.png",
+    image: "https://i.postimg.cc/bvCDF4NM/Adobe-Express-file.png",
     title: "Dragon Ballz Action Figure"
   },
   {
     id: 8,
-    image: "https://i.postimg.cc/XY8KBxDz/Chat-GPT-Image-Aug-2-2025-04-00-57-PM-1-1.png",
+    image: "https://i.postimg.cc/BnPV6fR5/Adobe-Express-file-1.png",
     title: "Doll's"
   },
   {
     id: 9,
-    image: "https://i.postimg.cc/XY8KBxDz/super-hero-bricks.png",
+    image: "https://i.postimg.cc/SNR8FWv3/Adobe-Express-file-4.png",
     title: "Super Hero Bricks"
   },
   {
     id: 10,
-    image: "https://i.postimg.cc/XY8KBxDz/character-bricks.png",
+    image: "https://i.postimg.cc/1RjXFbv7/Gemini-Generated-Image-2gyw412gyw412gyw.png",
     title: "Character Bricks"
   },
   {
     id: 11,
-    image: "https://i.postimg.cc/N0C1pFt8/Chat-GPT-Image-Aug-2-2025-03-49-34-PM-1-2-1.png",
+    image: "https://i.postimg.cc/NfGhg06W/Adobe-Express-file-5.png",
     title: "Key Chain"
   },
   {
     id: 12,
-    image: "https://i.postimg.cc/N0C1pFt8/Chat-GPT-Image-Aug-2-2025-03-49-34-PM-1-2-1.png",
+    image: "https://i.postimg.cc/dtv6kqBv/Adobe-Express-file-6.png",
     title: "Decorative Items"
   }
 ];
 
 const CategoryShowing = ({ 
-  title = "Popular Categories",
+  title = "Popular Categories ⭐",
   showAllText = "Show all",
   onShowAllClick 
 }) => {
